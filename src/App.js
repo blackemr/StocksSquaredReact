f43d4c5b9f0665d7c8a5fe8b818a1df032b60378
Ray Zhang <zhangray47@gmail.com>
2020-12-15 16:04:49 -0500

    changing the source

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Stocks Squared will live here soon
+          Stocks Squared will live here soon!
         </p>
       </header>
     </div>
